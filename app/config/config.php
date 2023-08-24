d7234b8da4f9b839586809d633b2a8527eb53d73
BagusDB <bagusdbr@gmail.com>
2023-08-24 08:20:01 +0700

    config

--- app/config/config.php
+++ app/config/config.php
@@ -1,8 +1,8 @@
 <?php
 
-define('base_url', 'http://smkn6jember.net/RPL1_17/public');
+define('base_url', 'http://siswa.smkn6jember.net/RPL1_17/public');
 
-define('DB_HOST', 'smkn6jember.net');
+define('DB_HOST', 'localhost');
 define('DB_USER', 'siswa');
 define('DB_PASS', 'siswa');
 define('DB_NAME', 'RPL1_17');
